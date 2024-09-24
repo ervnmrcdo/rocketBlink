936b140bf06756bb942020bc27ee6488f991035a
Ervin Mercado <ervinmercado31@gmail.com>
2024-09-24 20:56:11 +0800

    Update

--- src/app/api/actions/const.ts
+++
@@ -1,7 +0,0 @@
-import { PublicKey } from "@solana/web3.js";
-
-export const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey(
-	"7AiCz3SEtk45dFQxzziFUS3SJEQFtNB4TzuG4QzjqXAh", // devnet wallet
-);
-
-export const DEFAULT_SOL_AMOUNT: number = 0.5;
--- src/app/api/actions/playing-action/route.ts
+++ src/app/api/actions/playing-action/route.ts
@@ -1,19 +1,11 @@
 import { ActionGetResponse, ActionPostRequest, ActionPostResponse, ACTIONS_CORS_HEADERS, createPostResponse, ActionError } from "@solana/actions";
-import { clusterApiUrl, Connection, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
-import { DEFAULT_SOL_ADDRESS, DEFAULT_SOL_AMOUNT } from "./const";
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
 
 
 export const GET = async (req: Request) => {
 
 
 	const requestUrl = new URL(req.url);
-	// const { toPubkey } = validatedQueryParams(requestUrl);
-	//
-	// const baseHref = new URL(
-	// 	`/api/actions?to=${toPubkey.toBase58()}`,
-	// 	requestUrl.origin,
-	// ).toString();
-
 	const payload: ActionGetResponse = {
 		title: "Rocket Blink",
 		icon: new URL("/rocket.png", requestUrl.origin).toString(),
@@ -34,27 +26,17 @@ export const GET = async (req: Request) => {
 	return Response.json(payload, {
 		headers: ACTIONS_CORS_HEADERS,
 	});
-	// } catch (err) {
-	// 	let actionError: ActionError = { message: "An unknow error occured" };
-	// 	if (typeof err == "string") actionError.message = err;
-	// 	return Response.json(actionError, {
-	// 		status: 400,
-	// 		headers: ACTIONS_CORS_HEADERS,
-	// 	})
-	// }
 }
 
 
 export const OPTIONS = GET;
 
 export const POST = async (req: Request) => {
-	const requestUrl = new URL(req.url);
-	// const { amount, toPubkey } = validatedQueryParams(requestUrl);
 	const body: ActionPostRequest = await req.json();
 
 
-	const toPubkey = new PublicKey("7AiCz3SEtk45dFQxzziFUS3SJEQFtNB4TzuG4QzjqXAh")
 	const fromPubkey = new PublicKey(body.account);
+	const toPubkey = Keypair.generate();
 	const connection = new Connection(clusterApiUrl("devnet"));
 
 	const tx = new Transaction();
@@ -93,3 +75,5 @@ export const POST = async (req: Request) => {
 }
 
 
+
+}
