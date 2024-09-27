6d7e527f976ae36c948f8882fffe9af3fd697869
Ervin Mercado <ervinmercado31@gmail.com>
2024-09-27 15:24:16 +0800

    update

--- src/app/api/actions/start_action/route.ts
+++ src/app/api/actions/start_action/route.ts
@@ -8,7 +8,7 @@ export const GET = async (req: Request) => {
 	const requestUrl = new URL(req.url);
 	const payload: ActionGetResponse = {
 		title: "Rocket Blink",
-		icon: new URL("/rocket.png", requestUrl.origin).toString(),
+		icon: new URL("/rocket.jpg", requestUrl.origin).toString(),
 		description: "To the MOON!",
 		label: "Play",
 		links: {
